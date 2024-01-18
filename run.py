001b8378132b6012e221c4343f40977fc6f6052e
renantescaro <renantescaro@yahoo.com.br>
2024-01-17 23:26:06 -0300

    fix

--- run.py
+++ run.py
@@ -1,6 +1,22 @@
-import cloudinary
+import os
+import cloudinary.uploader
 from dotenv import load_dotenv
 
 load_dotenv()
 
-cloudinary.uploader.upload("usutau.jpg")
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+)
+
+cloudinary.uploader.upload(
+    "urutau.jpg",
+    public_id="urutau",
+    unique_filename=False,
+    overwrite=True,
+)
+
+url_image = cloudinary.CloudinaryImage("urutau").build_url()
+
+print(f"toma sua url: {url_image}")
